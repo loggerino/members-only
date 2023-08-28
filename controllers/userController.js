ba38b905aebf347be8d16094f63c905bf29d63f1
Chyisiang <chyisiang8@gmail.com>
2023-08-28 20:13:03 +0800

    sanitise form fields, async handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,25 @@
 const passport = require('passport');
 const User = require('../models/user');
+const { body, validationResult } = require('express-validator');
+const asyncHandler = require('express-async-handler');
 
 exports.signupGet = (req, res) => {
     res.render('signup', { title: 'Sign Up' });
 };
 
-exports.signupPost = async (req, res, next) => {
+exports.signupPost = asyncHandler(async (req, res, next) => {
     try {
+        await body('firstName').trim().notEmpty().withMessage('First name is required.').run(req);
+        await body('lastName').trim().notEmpty().withMessage('Last name is required.').run(req);
+        await body('email').trim().isEmail().withMessage('Invalid email address.').run(req);
+        await body('password').trim().isLength({ min: 6 }).withMessage('Password must be at least 6 characters long.').run(req);
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            req.flash('error', errors.array()[0].msg);
+            return res.redirect('/auth/signup');
+        }
+
         const { firstName, lastName, email, password } = req.body;
         const user = new User({ firstName, lastName, email, password });
         await user.save();
@@ -14,7 +27,7 @@ exports.signupPost = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-};
+});
 
 exports.loginGet = (req, res) => {
     res.render('login', { title: 'Log In' });
